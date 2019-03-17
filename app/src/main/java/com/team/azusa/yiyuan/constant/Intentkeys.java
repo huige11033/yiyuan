8e36dd346a4b69970bd297625979d8e8272db6f9
chenru <42888525@qq.com>
2019-03-17 12:58:08 +0800

    测试测试

---
+++ app/src/main/java/com/team/azusa/yiyuan/constant/Intentkeys.java
@@ -0,0 +1,10 @@
+package com.team.azusa.yiyuan.constant;
+
+/**
+ * @author : chenru
+ * @功能描述:
+ * @创建时间: 2019/3/17
+ */
+public interface Intentkeys {
+
+}
---
+++ app/src/main/java/com/team/azusa/yiyuan/event/UserChangeEvent.java
@@ -0,0 +1,12 @@
+package com.team.azusa.yiyuan.event;
+
+/**
+ * Created by Azusa on 2016/1/19.
+ */
+public class UserChangeEvent {
+
+    public UserChangeEvent() {
+
+    }
+
+}
